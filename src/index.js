0c7fb59116edf9518d20fc35fc5a0f089b367c45
wujiamian <wujiamian@xgd.com>
2024-05-19 16:03:13 +0800

    react requestIdleCallBack兼容

--- src/index.js
+++ src/index.js
@@ -1,29 +1,69 @@
-import React from 'react';
-const Comp = (
-  <div id="comp">
-    <p>text1</p>
-    <p>text2</p>
-    </div>
-)
-console.log(Comp)
+let oneFrameTime = 100 / 60 //16.6 浏览器每帧时间
+let dealTime
+let pendingCb
+const channel = new MessageChannel()
+window.requestIdleCallback = (cb, options) => {
 
-// document.getElementById('root')
-const render = (virtualDom,container) => {
-  const {type, props} = virtualDom
-  const dom = document.createElement(type) 
-  Object.keys(props).filter(prop => prop !=='children').forEach(prop => {
-    //将props 属性挂在dom结构上，但排除children属性
-    // 注意 这是简单对属性处理，还需要对style 事件进行特色处理
-    dom[prop] = props[prop]
+  window.requestAnimationFrame((rafTime) => {
+    // rafTime 可以看着一帧开始时间等于 performance.timing.navigationStart + performance.now()
+    // 一帧结束时间
+    dealTime = rafTime + oneFrameTime //
+    pendingCb = cb
+    channel.port1.postMessage('')
   })
-  // 如果children是数组，递归调用 render函数
-  // 注意这是react.16之前版本 stack 递归渲染方式，但会存在问题，如果子阶段很多，js执行时间过长，会操作页面卡顿
-  if(Array.isArray(props.children)) {
-     props.children.forEach(child => render(child, dom))
-  }else {
-    dom.textContent = props.children
-  }
-  container.appendChild(dom)
 }
 
-render(Comp, document.getElementById('root'))
+const timeRemaining = () =>  dealTime - performance.now()
+// 接受 port1发送给来的信息
+// 根据浏览器时间循环机制可知，当微任务执行完，浏览器开始渲染，渲染完成开始执行宏任务，即（messageChannel事件）
+channel.port2.onmessage = () => {
+  const currentTime = performance.now()
+  const didTimeout = dealTime <= currentTime
+  if (didTimeout || timeRemaining() > 0) {
+    if(pendingCb){
+        pendingCb({didTimeout,timeRemaining})
+    }
+  }
+}
+
+const sleep = (duration) => {
+   const start = Date.now() 
+   while (start + duration > Date.now()) {}
+}
+
+const queue = [
+  () => {
+    console.log('A开始')
+    sleep(20)
+    console.log('A结束')
+  },
+  () => {
+    console.log('B开始')
+    sleep(20)
+    console.log('B结束')
+  },
+]
+
+
+
+const workLoop = (dealTime) => {
+
+  console.log(`本帧剩余时间${dealTime.timeRemaining()}`)
+  // 如果有剩余时间 并且有任务 
+  while(dealTime.timeRemaining() > 0 && queue.length) {
+    performUnitOfWork()
+  }
+
+  // 如果没有剩余时间，当还有任务存在
+  if(queue.length) {
+    requestIdleCallback(workLoop)
+  }
+}
+
+const performUnitOfWork = () => {
+  const task = queue.shift()
+  task()
+}
+
+requestIdleCallback(workLoop)
+
